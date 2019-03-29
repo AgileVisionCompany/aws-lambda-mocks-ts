ed4f2648951c5237511b0e106a99181aab6b43d7
Artem Efimenko <aefimenko@agilevision.io>
2019-03-29 15:13:05 +0200

    Change default ApiGateway headers mock

--- src/events/api-gateway.ts
+++ src/events/api-gateway.ts
@@ -18,12 +18,8 @@ export const mockApiGatewayEvent = (
 ): APIGatewayProxyEvent => {
   return {
     path,
-    body: !!body
-      ? body instanceof String
-        ? body
-        : JSON.stringify(body)
-      : null,
-    headers: !!headers ? Object.assign(defaultHeaders, headers) : headers,
+    body: !!body ? body : null,
+    headers: !!headers ? Object.assign(defaultHeaders, headers) : defaultHeaders,
     multiValueHeaders: {},
     httpMethod: !!method ? method : "GET",
     isBase64Encoded: false,
@@ -44,26 +40,12 @@ export const mockApiGatewayEvent = (
 };
 
 const defaultHeaders = {
-  Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-  "Accept-Encoding": "gzip, deflate",
-  "Accept-Language": "en-us",
-  "CloudFront-Forwarded-Proto": "https",
-  "CloudFront-Is-Desktop-Viewer": "true",
-  "CloudFront-Is-Mobile-Viewer": "false",
-  "CloudFront-Is-SmartTV-Viewer": "false",
-  "CloudFront-Is-Tablet-Viewer": "false",
-  "CloudFront-Viewer-Country": "US",
-  Cookie:
-    "__gads=ID=d51d609e5753330d:T=1443694116:S=ALNI_MbjWKzLwdEpWZ5wR5WXRI2dtjIpHw; __qca=P0-179798513-1443694132017; _ga=GA1.2.344061584.1441769647",
-  Host: "xxx.execute-api.us-east-1.amazonaws.com",
-  "User-Agent":
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/601.6.17 (KHTML, like Gecko) Version/9.1.1 Safari/601.6.17",
-  Via: "1.1 c8a5bb0e20655459eaam174e5c41443b.cloudfront.net (CloudFront)",
-  "X-Amz-Cf-Id": "z7Ds7oXaY8hgUn7lcedZjoIoxyvnzF6ycVzBdQmhn3QnOPEjJz4BrQ==",
-  "X-Forwarded-For": "221.24.103.21, 54.242.148.216",
-  "X-Forwarded-Port": "443",
-  "X-Forwarded-Proto": "https"
+  "accept":"application/json",
+  "cache-control":"no-cache",
+  "host": "localhost",
+  "connection":"keep-alive"
 };
+
 const defaultApiGatewayEventRequestContext = (
   path: string,
   resourcePath?: string,
