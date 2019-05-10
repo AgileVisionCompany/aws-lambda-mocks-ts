c7ddb4998186e198d75695a6a631706bc532bcd7
Artem Efimenko <aefimenko@agilevision.io>
2019-05-10 12:19:36 +0300

    Update event api

--- dist/events/index.d.ts
+++ dist/events/index.d.ts
@@ -1,1 +1,3 @@
+import * as ApiGateway from "./api-gateway";
 export { mockApiGatewayEvent, mockLambdaContext, mockCallback } from "./api-gateway";
+export { ApiGateway };
--- dist/events/index.js
+++ dist/events/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var ApiGateway = require("./api-gateway");
+exports.ApiGateway = ApiGateway;
 var api_gateway_1 = require("./api-gateway");
 exports.mockApiGatewayEvent = api_gateway_1.mockApiGatewayEvent;
 exports.mockLambdaContext = api_gateway_1.mockLambdaContext;
